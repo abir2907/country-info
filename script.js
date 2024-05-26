ef268e54e48102ed596bf32b9ff9e78dfbd14f86
Abir Chodha <abirchodha1@gmail.com>
2024-05-27 00:15:03 +0530

    Create function getCountry for any country

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v2/name/india');
-request.send();
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
 
-request.addEventListener('load', function () {
-  const [, data] = JSON.parse(this.responseText);
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
 
-  const html = `<article class="country">
+    const html = `<article class="country">
   <img class="country__img" src="${data.flag}" />
   <div class="country__data">
     <h3 class="country__name">${data.name}</h3>
@@ -25,6 +26,10 @@ request.addEventListener('load', function () {
   </div>
 </article>`;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-});
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('spain');
+getCountryData('usa');
