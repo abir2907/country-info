94433ecc707a5712d86b3daa034782ced464fe47
Abir Chodha <abirchodha1@gmail.com>
2024-05-27 13:03:31 +0530

    Handle successfull promise using then

--- script.js
+++ script.js
@@ -23,33 +23,12 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-
-    // Render country 1
-    renderCountry(data);
-
-    // Get neighbour country (2)
-    const neighbour = data.borders?.[0];
-
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-    request2.send();
-
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-
-      // Render country 2
-      renderCountry(data2, 'neighbour');
-    });
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`).then(function (
+    response
+  ) {
+    console.log(response);
   });
 };
 
-getCountryAndNeighbour('spain');
+getCountryData('portugal');
