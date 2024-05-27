2f7f675380646b8bf9d4aebe5e5601b16c7c5201
Abir Chodha <abirchodha1@gmail.com>
2024-05-27 13:33:30 +0530

    Render neighbour country using promise .then() always returns a promise, and the next .then() will handle the success value of that promise, therefore we return fetch(neighbor) from then, which is a promise

--- script.js
+++ script.js
@@ -24,9 +24,18 @@ const renderCountry = function (data, className = '') {
 };
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+
+      // Country 2
+      const neighbour = data[0].borders?.[0];
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('portugal');
