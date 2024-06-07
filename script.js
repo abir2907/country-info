6712962a9c180d3a871b8412a06739221003da8d
Abir Chodha <abirchodha1@gmail.com>
2024-06-07 21:25:33 +0530

    Running promises in parallel using Promise.all()

--- script.js
+++ script.js
@@ -91,3 +91,17 @@ const whereAmI = async function () {
 };
 
 btn.addEventListener('click', whereAmI);
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const data = await Promise.all([
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
